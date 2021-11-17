ebe5bacaf0ac10368a9080a965b840861c623cb8
Roshan Parate <roshanparate1991@gmail.com>
2021-11-17 02:23:55 +0000

    searching and sorting

--- Algo-Searching/DFS.js
+++ Algo-Searching/DFS.js
@@ -148,7 +148,66 @@ class BinarySearchTree {
     }
     return list;
   }
+  BreadthFirstSearchR(queue, list) {
+    if (!queue.length) {
+      return list;
+    }
+    const currentNode = queue.shift();
+    list.push(currentNode.value);
+    
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+    
+    return this.BreadthFirstSearchR(queue, list);
+  }
+  DFTPreOrder(currentNode, list) {
+    return traversePreOrder(this.root, []);
+  }
+  DFTPostOrder(){
+    return traversePostOrder(this.root, []); 
+  }
+  DFTInOrder(){
+    return traverseInOrder(this.root, []);
+  } 
 }
+
+function traversePreOrder(node, list){
+  list.push(node.value);
+  if(node.left) {
+    traversePreOrder(node.left, list);
+  }
+  if(node.right) {
+    traversePreOrder(node.right, list);
+  }
+  return list;
+}
+
+function traverseInOrder(node, list){
+  if(node.left) {
+    traverseInOrder(node.left, list);
+  }
+  list.push(node.value);
+  if(node.right) {
+    traverseInOrder(node.right, list);
+  }
+  return list;
+}
+
+function traversePostOrder(node, list){
+  if(node.left) {
+    traversePostOrder(node.left, list);
+  }
+  if(node.right) {
+    traversePostOrder(node.right, list);
+  }
+  list.push(node.value);
+  return list;
+}
+
 
 const tree = new BinarySearchTree();
 tree.insert(9)
@@ -158,8 +217,14 @@ tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
+// tree.remove(170);
+// JSON.stringify(traverse(tree.root))
 
 console.log('BFS', tree.BreadthFirstSearch());
+console.log('BFS', tree.BreadthFirstSearchR([tree.root], []))
+console.log('DFSpre', tree.DFTPreOrder());
+console.log('DFSin', tree.DFTInOrder());
+console.log('DFSpost', tree.DFTPostOrder());
 
 //     9
 //  4     20
